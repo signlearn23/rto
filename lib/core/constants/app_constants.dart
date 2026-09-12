a0b408766bcec4420adac916f61de126cc61c8d2
signlearn23 <signlearnings@gmail.com>
2026-09-12 07:08:57 +0530

    Update app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -12,7 +12,7 @@ class AppConstants {
   static const String removeAdsPriceLabel = '₹39';
 
   // AdMob - TEST IDs (Google public test units). Replace before release.
-  static const String bannerAdUnitId = 'ca-app-pub-3699335518824613/7601380696';
+  static const String bannerAdUnitId = 'ca-app-pub-3940256099942544/6300978111';
   static const String rewardedAdUnitId = 'ca-app-pub-3699335518824613/8171531808';
 
   // Hive box names
