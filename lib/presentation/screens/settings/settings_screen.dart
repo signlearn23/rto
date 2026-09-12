f7cf99ec89e737e4d30a3ddd7168ac5ff4fa9041
signlearn23 <signlearnings@gmail.com>
2026-09-12 20:05:04 +0530

    Update settings_screen.dart

--- lib/presentation/screens/settings/settings_screen.dart
+++ lib/presentation/screens/settings/settings_screen.dart
@@ -5,7 +5,7 @@ import 'package:url_launcher/url_launcher.dart';
 import '../../../core/constants/app_constants.dart';
 import '../../providers/app_state_provider.dart';
 import '../../widgets/language_picker.dart';
-import '../onboarding/onboarding_selection_screen';
+import '../onboarding/onboarding_selection_screen.dart';
 import '../remove_ads/remove_ads_screen.dart';
 import 'simple_content_screen.dart';
 
