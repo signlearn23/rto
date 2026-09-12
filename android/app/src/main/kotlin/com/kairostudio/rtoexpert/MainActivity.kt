b94a8c8aa4b948240499af4b242e3061625774ec
signlearn23 <signlearnings@gmail.com>
2026-09-12 22:07:58 +0530

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/kairostudio/rtoexpert/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.kairostudio.rtoexpert
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GoogleMobileAdsPlugin.registerNativeAdFactory(
+            flutterEngine,
+            "advancedNativeAd", // must match factoryId in PracticeQuestionScreen
+            NativeAdFactoryImpl(context)
+        )
+    }
+
+    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
+        super.cleanUpFlutterEngine(flutterEngine)
+        GoogleMobileAdsPlugin.unregisterNativeAdFactory(flutterEngine, "advancedNativeAd")
+    }
+}
